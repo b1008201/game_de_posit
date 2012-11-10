e40a32f43ce3dfbe5130b5d9ae9df7b386014010
Micasa (b1008201) <b1008201@gmail.com>
2012-11-10 23:23:49 +0900

    修正

--- controllers/communities_controller.php
+++ controllers/communities_controller.php
@@ -2,55 +2,48 @@
 
 class CommunitiesController extends AppController{
 
-var$name = "Communities";
-var $helpers = array('Html');
-var $uses = array('Community');
+    var$name = "Communities";
+    var $helpers = array('Html');
+    var $uses = array('Community');
 
-function beforeFilter() {
+    function beforeFilter() {
         parent::beforeFilter();
         $this->layout = 'gdp_layout';
         $title_for_layout = 'GameDePosit';
         $this->set(compact('title_for_layout','body_for_layout'));
 
-}
+    }
 
-function index() { 
+    function index() {
 
-}
+    }
 
-function musicitem() {
-$this->set("referer", $this->referer());
-}
+    function musicitem() {
+        $this->set("referer", $this->referer());
+    }
 
-function tripitem() {
-	$this->set("referer", $this->referer());
-}
-
-
-function bbs() {
-if (($this->data)!=NULL) {
-if($this->data["Community"]["comment"]!=NUll){
-$comment["Community"]["comment"] = $this->data["Community"]["comment"];
-$comment["Community"]["username"] = $this->data["Community"]["username"];
-$this->Community->save($comment);
-$this->redirect("/communities/bbs");
-}
-}
+    function tripitem() {
+        $this->set("referer", $this->referer());
+    }
 
 
-$this->paginate = Array(
-		'limit' => 10,
-		'order' => array('Community.created' => 'desc'
-		));
-$this->set('all_bbs', $this->paginate());
+    function bbs() {
+        if (($this->data)!=NULL) {
+            if($this->data["Community"]["comment"]!=NUll){
+                $comment["Community"]["comment"] = $this->data["Community"]["comment"];
+                $comment["Community"]["username"] = $this->data["Community"]["username"];
+                $this->Community->save($comment);
+                $this->redirect("/communities/bbs");
+            }
+        }
 
 
-
-}
-
+        $this->paginate = Array(
+                'limit' => 5,
+                'order' => array('Community.created' => 'desc'
+                ));
+        $this->set('all_bbs', $this->paginate());
+    }
 
 }
-
-
-
 ?>
